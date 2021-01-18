58c56353ebfa476bbff5c2826718c6713efcd7c0
zhutianlong <zhutianlong95@163.com>
2021-01-18 22:25:21 +0800

    hearthstone card

--- src/views/FEArmory/Components/MagicCard/MagicCardView.vue
+++ src/views/FEArmory/Components/MagicCard/MagicCardView.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="magic-card">
     <div class="container">
-      <div class="cover">cover1</div>
-      <div class="back">红桃A</div>
+      <div class="cover"></div>
+      <div class="back rd"></div>
     </div>
     <div class="container">
-      <div class="cover">cover2</div>
-      <div class="back">红桃A</div>
+      <div class="cover"></div>
+      <div class="back bd"></div>
     </div>
     <div class="container">
-      <div class="cover">cover3</div>
-      <div class="back">红桃A</div>
+      <div class="cover"></div>
+      <div class="back d12"></div>
     </div>
   </div>
 </template>
@@ -39,24 +39,41 @@ export default {
   height: 400px;
   perspective: 1000px;
   cursor: pointer;
+  position: relative;
 }
 .cover {
   position: absolute;
   width: 100%;
   height: 100%;
-  background-color:blue;
   backface-visibility: hidden;
   transform: rotateY(0);
   transition: transform .3s ease-in-out;
+  background-image: url('../../../../assets/img/hearthstone1.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
 }
 .back {
   position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color:yellow;
+  width: 320px;
+  height: 450px;
   backface-visibility: hidden;
   transform: rotateY(-180deg);
   transition: transform .3s ease-in-out;
+  border-radius: 12px;
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  top: -42px;
+  left: 0;
+  position: relative;
+}
+.d12 {
+  background-image: url('../../../../assets/img/hearthstone2.png');
+}
+.rd {
+  background-image: url('../../../../assets/img/hearthstone3.png');
+}
+.bd {
+  background-image: url('../../../../assets/img/hearthstone4.png');
 }
 .container:hover .cover {
   transform: rotateY(180deg)
